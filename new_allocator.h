8db3d795351d0b2cf040504a338221d8c27ff012
zuobin Wang <244791255@qq.com>
2020-06-12 01:41:05 +0800

    feat:test and new_allocator

---
+++ new_allocator.h
@@ -0,0 +1,47 @@
+#ifndef NEW_ALLOCATOR_H
+#define NEW_ALLOCATOR_H
+
+#include <new>
+#include <iostream>
+#include <cstddef>
+#include <cstdlib>
+#include <climits>
+//allocator using new and delete
+template <class T>
+class New_Allocator
+{
+public:
+    //some define from demo
+    typedef T value_type;
+    typedef T *pointer;
+    typedef const T *const_pointer;
+    typedef T &reference;
+    typedef const T &const_reference;
+    typedef size_t size_type;
+    typedef ptrdiff_t diference_type;
+    typedef std::false_type propagate_on_container_copy_assignment;
+    typedef std::true_type propagate_on_container_move_assignment;
+    typedef std::true_type propagate_on_container_swap;
+    template <class U>
+    struct rebind
+    {
+        typedef New_Allocator<U> other;
+    };
+    //construct
+    New_Allocator()  = default;
+    New_Allocator(const New_Allocator &New_Allocator) = default;
+    New_Allocator(New_Allocator &&New_Allocator) = default;
+    template <class U>
+    New_Allocator(const New_Allocator<U> &other) noexcept;
+    //new operator for allocate
+    T* allocate(size_type n) {
+        auto tmp = (T*)(::operator new((size_type)(n * sizeof(T))));
+
+        if (tmp == 0)throw std::bad_alloc();
+        return tmp;
+    }
+    //delete operator foe deallocate
+    void deallocate(T* buf, size_type) {delete(buf); }
+
+};
+#endif
---
+++ testallocator.cpp
@@ -0,0 +1,170 @@
+
+#include "stdafx.h"
+#include "new_allocator.h"
+#include <ctime>
+#include <vector>
+#include <iostream>
+#include <string>
+
+#define INT 1
+#define FLOAT 2
+#define DOUBLE 3
+#define CLASS  4
+
+#define NEW_ALLOCATOR 10001
+#define MY_ALLOCATOR 10002
+#define STD_ALLOCATOR 10003
+class vecWrapper
+{
+public:
+	vecWrapper() {
+		m_pVec = NULL;
+		m_type = INT;
+	}
+	virtual ~vecWrapper()	{
+	}
+public:
+	void setPointer(int type, void *pVec) { m_type = type; m_pVec = pVec; }
+	virtual void visit(int index) = 0;
+	virtual int size() = 0;
+	virtual void resize(int size) = 0;
+	virtual bool checkElement(int index, void *value) = 0;
+	virtual void setElement(int idex, void *value) = 0;
+protected: 
+	int m_type;
+	void *m_pVec;
+};
+
+template <typename T, template<class> class anyAllocator>
+class vecWrapperT : public vecWrapper
+{
+public:
+	vecWrapperT(int type, std::vector<T, anyAllocator<T> > *pVec)
+	{ 
+		m_type = type;
+		m_pVec = pVec;
+	}
+	virtual ~vecWrapperT() {
+		if (m_pVec)
+			delete ((std::vector<T, anyAllocator<T> > *)m_pVec);
+	}
+public:
+	virtual void visit(int index)
+	{
+		T temp = (*(std::vector<T, anyAllocator<T> > *)m_pVec)[index];
+	}
+	virtual int size()
+	{
+		return ((std::vector<T, anyAllocator<T> > *)m_pVec)->size();
+	}
+	virtual void resize(int size)
+	{
+		((std::vector<T, anyAllocator<T> > *)m_pVec)->resize(size);
+	}
+	virtual bool checkElement(int index, void *pValue)
+	{
+		T temp = (*(std::vector<T, anyAllocator<T> > *)m_pVec)[index];
+		if (temp == (*((T *)pValue)))
+			return true;
+		else
+			return false;
+	}
+
+	virtual void setElement(int index, void *value)
+	{
+		(*(std::vector<T, anyAllocator<T> > *)m_pVec)[index] = *((T *)value);
+	}
+};
+
+class myObject
+{
+public:
+	myObject() : m_X(0), m_Y(0) {}
+	myObject(int t1, int t2) :m_X(t1), m_Y(t2) {}
+	myObject(const myObject &rhs) { m_X = rhs.m_X; m_Y = rhs.m_Y;}
+	~myObject() { /*std::cout << "my object destructor called" << std::endl;*/ }
+	bool operator == (const myObject &rhs)
+	{
+		if ((rhs.m_X == m_X) && (rhs.m_Y == m_Y))
+			return true;
+		else
+			return false;
+	}
+protected:
+	int m_X;
+	int m_Y;
+};
+
+#define TESTSIZE 10000
+
+template <template<class> class anyAllocator>
+class test {
+   public:
+
+    void testMain(int mode) {
+		clock_t start;
+		
+		std::string mm = mode==10001?"New_Allocator":(mode==10002?"My_Allocator":"std_Allocator");
+		start = clock();
+		vecWrapper **testVec;
+	testVec = new vecWrapper*[TESTSIZE];
+
+	int tIndex, tSize;
+	//test allocator
+	for (int i = 0; i < TESTSIZE - 4; i++)
+	{
+		tSize = (int)((float)rand()/(float)RAND_MAX * 10000);
+		vecWrapperT<int,anyAllocator> *pNewVec = new vecWrapperT<int,anyAllocator>(INT, new std::vector<int, anyAllocator<int>>(tSize));
+		testVec[i] = (vecWrapper *)pNewVec;
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		tSize = (int)((float)rand() / (float)RAND_MAX * 10000);
+		vecWrapperT<myObject,anyAllocator> *pNewVec = new vecWrapperT<myObject,anyAllocator>(CLASS, new std::vector<myObject, anyAllocator<myObject>>(tSize));
+		testVec[TESTSIZE - 4 + i] = (vecWrapper *)pNewVec;
+	}
+	
+	//test resize
+	for (int i = 0; i < 100; i++)
+	{
+		tIndex = (int)((float)rand() / (float)RAND_MAX * (float)TESTSIZE);
+		tSize = (int)((float)rand() / (float)RAND_MAX * (float)TESTSIZE);
+		testVec[tIndex]->resize(tSize);
+	}
+
+	//test assignment
+	tIndex = (int)((float)rand() / (float)RAND_MAX * (TESTSIZE - 4 - 1));
+	int tIntValue = 10;
+	testVec[tIndex]->setElement(testVec[tIndex]->size()/2, &tIntValue);
+	if (!testVec[tIndex]->checkElement(testVec[tIndex]->size()/ 2, &tIntValue))
+		std::cout<< "incorrect assignment in vector %d\n" << tIndex << std::endl;
+
+	tIndex = TESTSIZE - 4 + 3;
+	myObject tObj(11,15);
+	testVec[tIndex]->setElement(testVec[tIndex]->size()/2, &tObj);
+	if (!testVec[tIndex]->checkElement(testVec[tIndex]->size()/2, &tObj))
+		std::cout << "incorrect assignment in vector %d\n" << tIndex << std::endl;
+	
+	myObject tObj1(13, 20);
+	testVec[tIndex]->setElement(testVec[tIndex]->size()/2, &tObj1);
+	if (!testVec[tIndex]->checkElement(testVec[tIndex]->size() / 2, &tObj1))
+		std::cout << "incorrect assignment in vector " << tIndex << " for object (13,20)" << std::endl;
+ 	std::cout <<mm << (clock() - start) * 1.0 / CLOCKS_PER_SEC << " seconds"<< std::endl;
+	start = clock();
+	for (int i = 0; i < TESTSIZE; i++)
+		delete testVec[i];
+
+	delete []testVec;
+	 std::cout <<mm << "delete"<< (clock() - start) * 1.0 / CLOCKS_PER_SEC << " seconds"<< std::endl;
+	}
+};
+
+int main()
+{
+	test<std::allocator> test1;
+	test1.testMain(STD_ALLOCATOR);
+	test<New_Allocator>test2;
+	test2.testMain(NEW_ALLOCATOR);
+}
+
